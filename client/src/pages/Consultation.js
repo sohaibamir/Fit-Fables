07e79a40e8d5716e16df4ae4b9ff1619bb2885f9
Dell <sohaib.binamir15@gmail.com>
2023-06-15 00:03:22 +0500

    changes

--- client/src/pages/Consultation.js
+++ client/src/pages/Consultation.js
@@ -6,6 +6,7 @@ import {
   bookAppointment,
   getAllDoctorsAdmin,
   updateAppointmentHistory,
+  isAuthenticated,
 } from "../api/api";
 import {
   AiFillClockCircle,
@@ -21,6 +22,8 @@ import axios from "axios";
 const Consultation = () => {
   const [doctors, setDoctors] = useState([]);
   const toast = useToast();
+  const email = isAuthenticated().email;
+  console.log(email);
 
   const handlePayment = async (token, doctorId, price) => {
     try {
@@ -75,6 +78,9 @@ const Consultation = () => {
 
   const loggedInUser = JSON.parse(localStorage.getItem("jwt"));
   const userId = loggedInUser?._id;
+  const filteredDoctors = doctors.filter((doctor) =>
+    doctor.appointments.every((app) => app.email !== email)
+  );
 
   return (
     <>
@@ -103,7 +109,7 @@ const Consultation = () => {
           </div>
           <div className="container">
             <div className="row">
-              {doctors.map((doctor, index) => (
+              {filteredDoctors.map((doctor, index) => (
                 <div className="col-md-3 col-sm-4 mt-5 doctor-box" key={index}>
                   <div
                     className="team-thumb wow fadeInUp"
